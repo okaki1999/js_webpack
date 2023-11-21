45c519355d6d1968cd1de967ab7ea3d074cd615e
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2023-11-21 15:13:49 +0900

    保存されたデータの取得

--- js_webpack_project_starter/src/js/modules/todo.js
+++ js_webpack_project_starter/src/js/modules/todo.js
@@ -28,11 +28,14 @@ function getTodoData(){
 
 function createTodoElement(todo){
     const todoItem = document.createElement('li');
+    todoItem.classList.add('td-item');
     const todoContent = document.createElement('p');
+    todoContent.classList.add('td-content');
     todoContent.textContent = todo.content;
     todoItem.appendChild(todoContent);
 
     const btnContainer = document.createElement('div');
+    btnContainer.classList.add('td-btn-container');
     const btn = document.createElement('img');
     btn.classList.add('td-btn');
     const upBtn = btn.cloneNode(false);
@@ -40,12 +43,16 @@ function createTodoElement(todo){
 
 
     if(!todo.isDone){
+        upBtn.classList.add('edit-btn');
+        btn.classList.add('isDone-btn');
         btn.setAttribute('src', './images/todo_button/ok.png');
         btnContainer.appendChild(btn);
         btnContainer.appendChild(upBtn);
         todoItem.appendChild(btnContainer);
         todosUl.appendChild(todoItem);
     }else{
+        upBtn.classList.add('undo-btn');
+        btn.classList.add('delete-btn');
         btn.setAttribute('src', './images/todo_button/cancel.png');
         btnContainer.appendChild(btn);
         btnContainer.appendChild(upBtn);
