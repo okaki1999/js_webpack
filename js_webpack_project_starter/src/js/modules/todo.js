a52f7ece3a5296d729d956d84374fae6a5b32f0b
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2023-11-21 17:35:44 +0900

    todoapp実装完了

--- js_webpack_project_starter/src/js/modules/todo.js
+++ js_webpack_project_starter/src/js/modules/todo.js
@@ -2,6 +2,8 @@ const addForm = document.querySelector('.td-add-form');
 const addInput = document.querySelector('.td-add-input');
 const todosUl = document.querySelector('.todos');
 const donesUl = document.querySelector('.dones');
+const searchForm = document.querySelector('.td-search-form');
+const searchInput = document.querySelector('.td-search-input');
 
 let todoData = [];
 
@@ -9,30 +11,31 @@ addForm.addEventListener('submit', e => {
     e.preventDefault();
     let todoObj = {
         content: addInput.value.trim(),
-        idDone: false
+        isDone: false
     };
-    if(todoObj.content){
+    if (todoObj.content) {
         todoData.push(todoObj);
     }
-    addInput.value =  '';
+    addInput.value = '';
     updateLS();
-});
+    updateTodo();
+})
 
-function updateLS(){
-    localStorage.setItem('myTodo',JSON.stringify(todoData));
+function updateLS() {
+    localStorage.setItem('myTodo', JSON.stringify(todoData));
 }
 
-function getTodoData(){
-    return JSON.parse(localStorage.getItem('myTodo'));
+function getTodoData() {
+    return JSON.parse(localStorage.getItem('myTodo')) || [];
 }
 
-function createTodoElement(todo){
-    const todoItem = document.createElement('li');
-    todoItem.classList.add('td-item');
-    const todoContent = document.createElement('p');
-    todoContent.classList.add('td-content');
-    todoContent.textContent = todo.content;
-    todoItem.appendChild(todoContent);
+function createTodoElement(todo) {
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('td-item');
+        const todoContent = document.createElement('p');
+        todoContent.classList.add('td-content');
+        todoContent.textContent = todo.content;
+        todoItem.appendChild(todoContent);
 
     const btnContainer = document.createElement('div');
     btnContainer.classList.add('td-btn-container');
@@ -41,8 +44,7 @@ function createTodoElement(todo){
     const upBtn = btn.cloneNode(false);
     upBtn.setAttribute('src', './images/todo_button/up.png');
 
-
-    if(!todo.isDone){
+    if (!todo.isDone) {
         upBtn.classList.add('edit-btn');
         btn.classList.add('isDone-btn');
         btn.setAttribute('src', './images/todo_button/ok.png');
@@ -50,7 +52,7 @@ function createTodoElement(todo){
         btnContainer.appendChild(upBtn);
         todoItem.appendChild(btnContainer);
         todosUl.appendChild(todoItem);
-    }else{
+    } else {
         upBtn.classList.add('undo-btn');
         btn.classList.add('delete-btn');
         btn.setAttribute('src', './images/todo_button/cancel.png');
@@ -59,7 +61,49 @@ function createTodoElement(todo){
         todoItem.appendChild(btnContainer);
         donesUl.appendChild(todoItem);
     }
+
+    todoItem.addEventListener('click', e => {
+        if (e.target.classList.contains('isDone-btn')) {
+            todo.isDone = true;
+        }
+        if (e.target.classList.contains('undo-btn')) {
+            todo.isDone = false;
+        }
+        if (e.target.classList.contains('edit-btn')) {
+            addInput.value = e.target.parentElement.previousElementSibling.textContent;
+            todoData = todoData.filter(data => data !== todo);
+            addInput.focus();
+        }
+        if (e.target.classList.contains('delete-btn')) {
+            todoData = todoData.filter(data => data !== todo);
+        }
+        updateLS();
+        updateTodo();
+    })
 }
 
-createTodoElement({content:'remains to do', isDone:false})
-createTodoElement({content:'already done', isDone:true})
+function updateTodo() {
+    todosUl.innerHTML = '';
+    donesUl.innerHTML = '';
+    todoData = getTodoData();
+    todoData.forEach(todo => {
+        createTodoElement(todo);
+    })
+}
+
+updateTodo();
+
+searchForm.addEventListener('submit', () => {
+    e.preventDefault();
+})
+
+searchInput.addEventListener('keyup',() => {
+    const searchWord = searchInput.value.trim().toLowerCase();
+    const todoItems = document.querySelectorAll('.td-item');
+    todoItems.forEach(todoItem => {
+        todoItem.classList.remove('hide');
+        if (!todoItem.textContent.toLowerCase().includes(searchWord)){
+            todoItem.classList.add('hide');
+        }
+    })
+})
