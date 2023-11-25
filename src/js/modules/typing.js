6489555308381b797237e908572ae600ad8c37d2
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2023-11-26 01:22:37 +0900

    Enterのエラー解消

--- src/js/modules/typing.js
+++ src/js/modules/typing.js
@@ -3,16 +3,25 @@ const typingGame = document.querySelector('#ty-game');
 const titleTime = document.querySelector('#ty-title-time');
 const timer = document.querySelector('#ty-timer');
 const timeSelectEl = document.querySelector('.ty-time-select');
+const typing = document.querySelector('#typing');
+const backToStart = document.querySelector('#ty-back-to-start');
+const resultContainer = document.querySelector('#ty-result-container');
 
 let timelimit = 30;
 let remainingTime;
+let isActive = false;
+let isPlaying = false;
 
 timeSelectEl.addEventListener('change', () =>{
     timelimit = timeSelectEl.value;
 })
 window.addEventListener('keypress', e => {
-    if(e.key === 'Enter'){
+    isActive = typing.classList.contains('active');
+
+    if(e.key === 'Enter' && isActive && !isPlaying){
         start();
+        isActive = false;
+        isPlaying = true;
     }
     return;
 })
@@ -24,3 +33,10 @@ function start(){
     remainingTime = timelimit;
     timer.textContent = remainingTime;
 }
+
+backToStart.addEventListener('click', () => {
+    typingGame.classList.remove('show');
+    startPage.classList.add('show');
+    resultContainer.classList.remove('show');
+    isPlaying = false;
+})
