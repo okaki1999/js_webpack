4138b1370ced4b0ec206a7e4e9b68dfee2bf9f86
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2023-11-29 15:23:42 +0900

    typingアプリ実装完了

--- src/js/modules/typing.js
+++ src/js/modules/typing.js
@@ -9,6 +9,8 @@ const resultContainer = document.querySelector('#ty-result-container');
 const textarea = document.querySelector('#ty-textarea');
 const quote = document.querySelector('#ty-quote');
 const author = document.querySelector('#ty-author-name');
+const LPM = document.querySelector('#ty-LPM');
+const quoteReview = document.querySelector('#ty-quote-review');
 
 let timelimit = 30;
 let remainingTime;
@@ -16,10 +18,13 @@ let isActive = false;
 let isPlaying = false;
 let intervalId = null;
 let quotes;
+let typedCount;
+let LPMCount;
 
-timeSelectEl.addEventListener('change', () =>{
+timeSelectEl.addEventListener('change', () => {
     timelimit = timeSelectEl.value;
 })
+
 window.addEventListener('keypress', e => {
     isActive = typing.classList.contains('active');
 
@@ -31,14 +36,16 @@ window.addEventListener('keypress', e => {
     return;
 })
 
-function start(){
+async function start(){
     startPage.classList.remove('show');
     typingGame.classList.add('show');
     titleTime.textContent = timelimit;
     remainingTime = timelimit;
     timer.textContent = remainingTime;
-    textarea.focus();
+    await fetchAndRenderQuotes();
     textarea.disabled = false;
+    textarea.focus();
+    typedCount = 0;
 
     intervalId = setInterval(() => {
         remainingTime -= 1;
@@ -59,18 +66,30 @@ backToStart.addEventListener('click', () => {
 function showResult(){
     textarea.disabled = true;
     clearInterval(intervalId);
-    setTimeout(() => {
-        resultContainer.classList.add('show');
+    LPMCount = remainingTime === 0 ? Math.floor(typedCount * 60/timelimit):
+        Math.floor(typedCount * 60/(timelimit - remainingTime));
+        quoteReview.innerHTML = `${quotes.quote} <br>--- ${quotes.author}`;
+        let count = 0;
+        setTimeout(() => {
+            resultContainer.classList.add('show');
+            const countup = setInterval(()=>{
+                    LPM.textContent = count;
+                    count += 1;
+                    if(count >= LPMCount){
+                        clearInterval(countup);
+                    }
+                }, 20)
     }, 1000);
 }
 
 async function fetchAndRenderQuotes(){
+    quote.innerHTML = '';
+    textarea.value = '';
     const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
     const response = await fetch(RANDOM_QUOTE_API_URL);
     const data = await response.json();
 
     quotes = {quote: data.content, author: data.author};
-    console.log(quotes);
 
     quotes.quote.split('').forEach(letter => {
         const span = document.createElement('span');
@@ -78,8 +97,32 @@ async function fetchAndRenderQuotes(){
         quote.appendChild(span);
     })
     author.textContent = quotes.author;
-    console.log(quote);
-    console.log(author);
 }
 
-fetchAndRenderQuotes();
+
+textarea.addEventListener('input', () => {
+    let inputArray = textarea.value.split('');
+    let spans = quote.querySelectorAll('span');
+    spans.forEach(span => {
+        span.className = '';
+    })
+    typedCount = 0;
+    inputArray.forEach((letter, index) => {
+        if(letter === spans[index].textContent){
+            spans[index].classList.add('correct');
+            if(spans[index].textContent !== ' '){
+                typedCount += 1;
+            }
+        }else{
+            spans[index].classList.add('wrong');
+            if(spans[index].textContent === ' '){
+                spans[index].classList.add('bar');
+            }
+
+        }
+    })
+    if(spans.length === inputArray.length &&
+        [...spans].every(span => span.classList.contains('correct'))){
+            showResult();
+        }
+})
